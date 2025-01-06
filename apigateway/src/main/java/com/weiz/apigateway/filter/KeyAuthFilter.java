eaf1e4cc8d6af6021c3aeebbc80898c708a5cb70
chaoweiz <luutrieuvi2003@gmail.com>
2025-01-06 14:04:26 +0700

    feat: add rate limiting for gateway

---
+++ apigateway/src/main/java/com/weiz/apigateway/filter/KeyAuthFilter.java
@@ -0,0 +1,44 @@
+package com.weiz.apigateway.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KeyAuthFilter extends AbstractGatewayFilterFactory<KeyAuthFilter.Config> {
+
+    @Value("${apiKey}")
+    private String apiKey;
+
+    public KeyAuthFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(KeyAuthFilter.Config config) {
+
+        return (exchange, chain) -> {
+          if (!exchange.getRequest().getHeaders().containsKey("apiKey")) {
+            throw new RuntimeException("missing authorization information");
+          }
+
+          System.out.println(apiKey);
+
+          String key = exchange.getRequest().getHeaders().getFirst("apiKey");
+
+            assert key != null;
+            if (!key.equals(apiKey)) {
+              throw new RuntimeException("wrong authorization information");
+          }
+
+          ServerHttpRequest request = exchange.getRequest();
+          return chain.filter(exchange.mutate().request(request).build());
+        };
+    }
+
+    public static class Config {
+
+    }
+}
